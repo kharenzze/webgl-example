ddc16ffb7006157f24a9ceb3aecb8af25064d8bd
Paolo <str237@gmail.com>
2021-09-06 21:51:35 +0200

    Import shader

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 //index.ts
 //
 import './main.css'
+import vert from './sample.vert'
+import frag from './sample.frag'
 
 const canvas = document.getElementById('canvas')
 const gl = canvas.getContext("experimental-webgl");
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-console.log(gl);
-console.log(canvas.width);
+
 
 gl.viewport(0, 0, canvas.width, canvas.height);
 gl.clearColor(0, 0.5, 0, 1);
