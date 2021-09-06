36447a63f63d5a5af5aa16c7f0616da5ee2f5302
Paolo <str237@gmail.com>
2021-09-06 18:36:49 +0200

    Full width. Clear screen

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@
 //
 import './main.css'
 
-console.log("hello world")
+const canvas = document.getElementById('canvas')
+const gl = canvas.getContext("experimental-webgl");
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+console.log(gl);
+console.log(canvas.width);
+
+gl.viewport(0, 0, canvas.width, canvas.height);
+gl.clearColor(0, 0.5, 0, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
